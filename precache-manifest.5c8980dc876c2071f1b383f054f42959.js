d20a3e6a99c6ffedfa4afae234002ffd24b664da
Johan Mouchet <mouchet.johan@gmail.com>
2020-10-06 16:16:11 +1100

    Updates

---
+++ precache-manifest.5c8980dc876c2071f1b383f054f42959.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a07c067073a38120c6a5e54fadb7f6d5",
+    "url": "/broccoli-co/index.html"
+  },
+  {
+    "revision": "685eccf59c886986c1f2",
+    "url": "/broccoli-co/static/css/main.0bac7fac.chunk.css"
+  },
+  {
+    "revision": "2b6be982839d06bc9432",
+    "url": "/broccoli-co/static/js/2.8c545294.chunk.js"
+  },
+  {
+    "revision": "7dc1957d9a346b613dbd2a0b55157052",
+    "url": "/broccoli-co/static/js/2.8c545294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685eccf59c886986c1f2",
+    "url": "/broccoli-co/static/js/main.2a21e091.chunk.js"
+  },
+  {
+    "revision": "4a400788124ba5c37c53",
+    "url": "/broccoli-co/static/js/runtime-main.0df09dc2.js"
+  },
+  {
+    "revision": "28c8c0da7d98924a9d4830f48576fed5",
+    "url": "/broccoli-co/static/media/check-circle.28c8c0da.svg"
+  },
+  {
+    "revision": "2e49826ad029d82a022694d71b80c84c",
+    "url": "/broccoli-co/static/media/exclamation-circle.2e49826a.svg"
+  },
+  {
+    "revision": "409d1431f6d06f0823b75600818bc1bc",
+    "url": "/broccoli-co/static/media/exclamation-triangle.409d1431.svg"
+  },
+  {
+    "revision": "581027b4ee922dc63727c22b553af102",
+    "url": "/broccoli-co/static/media/logo.581027b4.svg"
+  },
+  {
+    "revision": "35e06264228b8c9666e85286ea6cc6a7",
+    "url": "/broccoli-co/static/media/x.35e06264.svg"
+  }
+]);
